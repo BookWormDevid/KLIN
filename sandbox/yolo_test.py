a77e8eb3797f94f813f1851fad70a72ffb2c154c
BookWormDevid <Kot.d.a@icloud.com>
2026-02-07 16:46:44 +0300

    bs

---
+++ sandbox/yolo_test.py
@@ -0,0 +1,10 @@
+from pathlib import Path
+
+from ultralytics import YOLO
+
+parent = Path(__file__).resolve().parent
+
+model = YOLO(parent / "models/Suspicious_Activities_nano.pt")
+
+
+model.predict(source=parent / "videos/fi004.mp4", show=True, conf=0.6)
