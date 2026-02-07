0923b2134e656d96f80e9c2fefc096796f27c9e0
BookWormDevid <Kot.d.a@icloud.com>
2026-02-07 17:06:07 +0300

    Restructuring the project style

--- sandbox/yolo_test.py
+++ sandbox/yolo_test.py
@@ -4,7 +4,7 @@ from ultralytics import YOLO
 
 parent = Path(__file__).resolve().parent
 
-model = YOLO(parent / "models/yolo26n.pt")
+model = YOLO(parent / "models/yolo_small_weights.pt")
 
 
 model.predict(source=parent / "videos/fi004.mp4", show=True, conf=0.6)
