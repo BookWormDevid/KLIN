5a8e8cf48b2af1b6342fa6594a9531ae02ce6f42
BookWormDevid <Kot.d.a@icloud.com>
2026-03-02 12:31:46 +0300

    removed psycopg2, crashes in docker and linux.

--- sandbox/get_metrics.py
+++ sandbox/get_metrics.py
@@ -1,9 +1,9 @@
-import mlflow
-import pathlib
-from mlflow.tracking import MlflowClient
-import psycopg2
 import json
-from datetime import datetime
+import pathlib
+
+from mlflow.tracking import MlflowClient
+
+import mlflow
 
 BASE_DIR = pathlib.Path(__file__).resolve().parent.parent
 mlflow_db_path = BASE_DIR / "mlflow" / "mlflow.db"
@@ -16,9 +16,9 @@ RUN_ID = "2fd3f5fed4a54019a050ad1b769b11bb"
 
 run = client.get_run(RUN_ID)
 
-metrics_dict = run.data.metrics      # dict: metric_name → last_value
-params_dict  = run.data.params       # dict: param_name → value
-tags         = run.data.tags
+metrics_dict = run.data.metrics  # dict: metric_name → last_value
+params_dict = run.data.params  # dict: param_name → value
+tags = run.data.tags
 
 print("\nMetrics (last values):")
 for k, v in metrics_dict.items():
@@ -33,24 +33,24 @@ for k, v in params_dict.items():
 # ───────────────────────────────────────────────
 
 DB_CONFIG = {
-    "dbname":   "ml",
-    "user":     "postgres",
+    "dbname": "ml",
+    "user": "postgres",
     "password": "postgres",
-    "host":     "localhost",
-    "port":     "5432"
+    "host": "localhost",
+    "port": "5432",
 }
 
 # Преобразуем словари в JSON-строки
 metrics_json = json.dumps(metrics_dict, ensure_ascii=False)
-params_json  = json.dumps(params_dict,  ensure_ascii=False)
+params_json = json.dumps(params_dict, ensure_ascii=False)
 
 # добавить просчёт precision, recall, PSI между train_probs и prod_probs
-
+'''
 # Подключение и вставка
-conn = psycopg2.connect(**DB_CONFIG)
 cur = conn.cursor()
 
-cur.execute("""
+cur.execute(
+    """
     INSERT INTO metrics (
         metrics,
         params,
@@ -60,14 +60,13 @@ cur.execute("""
         %(params)s,
         %(created_at)s
     )
-""", {
-    "metrics":    metrics_json,
-    "params":     params_json,
-    "created_at": datetime.utcnow()
-})
+""",
+    {"metrics": metrics_json, "params": params_json, "created_at": datetime.utcnow()},
+)
 
-conn.commit()
+#conn.commit()
 cur.close()
-conn.close()
+# conn.close()
 
 print("Запись выполнена → один ряд с полными словарями metrics и params в JSON")
+'''
