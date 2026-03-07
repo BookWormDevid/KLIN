a80d617254880a1daa619187ceca98ed30c9507b
Meksiner <meksiner@outlook.com>
2026-03-07 18:26:33 +0300

    airflow

--- airflow/dags/test_dag.py
+++ airflow/dags/test_dag.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 
-from airflow.operators.bash import BashOperator  # type: ignore
+from airflow.providers.standard.operators.bash import BashOperator
 
 from airflow import DAG
 
@@ -8,7 +8,6 @@ from airflow import DAG
 with DAG(
     dag_id="test_dag",
     start_date=datetime(2024, 1, 1),
-    schedule_interval=None,  # type: ignore
     catchup=False,
 ) as dag:
     t1 = BashOperator(task_id="hello", bash_command="echo Hello Airflow 3!")
